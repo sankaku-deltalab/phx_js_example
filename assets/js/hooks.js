d1937b728320c2ad24cef36fa209178c3609b2df
Sankaku <sankaku_dlt.45631@outlook.jp>
2022-12-14 15:35:31 +0000

    Add component-wised client to server messaging

---
+++ assets/js/hooks.js
@@ -0,0 +1,18 @@
+export const Hooks = {}
+Hooks.ClientSider = {
+  mounted() {
+    console.log("ClientSider mounted");
+    this.pushEvent("client_sider_mounted", {id: this.el.id})
+
+    this.tickId = setInterval(() => {
+      console.log("ClientSider still alive");
+      this.pushEvent("client_sider_still_alive", {id: this.el.id})
+    }, 1000)
+  },
+
+  destroyed() {
+    console.log("ClientSider destroyed");
+    this.pushEvent("client_sider_destroyed", {id: this.el.id})
+    clearInterval(this.tickId)
+  }
+}
--- lib/phx_js_example_web/live/pushing_live/index.ex
+++ lib/phx_js_example_web/live/pushing_live/index.ex
@@ -1,9 +1,11 @@
 defmodule PhxJsExampleWeb.PushingLive.Index do
   use PhxJsExampleWeb, :live_view
   use Phoenix.HTML
+  import Phoenix.LiveView
 
   @impl true
   def mount(_params, _session, socket) do
+    socket = socket |> assign(show_client_sider: false)
     {:ok, socket}
   end
 
@@ -30,7 +32,15 @@ defmodule PhxJsExampleWeb.PushingLive.Index do
   end
 
   @impl true
-  def handle_event(_event, _params, socket) do
+  def handle_event("toggle_client_sider", _params, socket) do
+    socket = socket |> update(:show_client_sider, &(not &1))
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event(event, params, socket) do
+    IO.inspect(%{event: event, params: params})
+    IO.inspect(socket.assigns)
     {:noreply, socket}
   end
 end
